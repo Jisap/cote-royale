c54ad5a33033c9072537e969118d43452fe750d6
Jorge Matos <jisapjisap@gmail.com>
2025-05-21 18:31:30 +0200

    product feature

--- src/slices/ProductFeature/index.tsx
+++ src/slices/ProductFeature/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { Bounded } from "@/components/Bounded";
 
 /**
  * Props for `ProductFeature`.
@@ -13,40 +15,17 @@ export type ProductFeatureProps =
  */
 const ProductFeature: FC<ProductFeatureProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for product_feature (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
-    </section>
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.description} />
+      <PrismicNextImage field={slice.primary.image} />
+      <PrismicNextLink field={slice.primary.fragance}>
+        Link
+      </PrismicNextLink>
+    </Bounded>
   );
 };
 
