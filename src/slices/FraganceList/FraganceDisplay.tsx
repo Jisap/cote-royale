1eac0b5b89e5601d10d3d63436e266d6015f451e
Jorge Matos <jisapjisap@gmail.com>
2025-05-23 16:23:29 +0200

    fix fadeIn

--- src/slices/FraganceList/FraganceDisplay.tsx
+++ src/slices/FraganceList/FraganceDisplay.tsx
@@ -21,6 +21,8 @@ export const FranganceDisplay = async({ id }: FranganceDisplayProps) => {
   return (
     <FadeIn
       className="relative z-20 grid min-h-[85vh] w-full translate-y-20 items-center justify-items-start border border-white/10 p-4 text-left md:p-14 lg:p-20"
+      vars={{ duration: 2.5 }}
+      start="top 50%"
     >
       <div className="absolute inset-0 z-0">
         <PrismicNextImage 
@@ -33,7 +35,11 @@ export const FranganceDisplay = async({ id }: FranganceDisplayProps) => {
         />
       </div>
 
-      <FadeIn className="relative z-10 grid translate-y-8">
+      <FadeIn 
+        className="relative z-10 grid translate-y-8"
+        vars={{ duration: 3, delay: .8 }}
+        start="top 50%"  
+      >
         <h3 className="font-display mb-3 text-5xl md:text-6xl lg:text-7xl">
           <PrismicText 
             field={fragance.data.title}
