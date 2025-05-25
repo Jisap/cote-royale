e821f6f94d89b6aaec33a77afcc7acc75cfb1ff0
Jorge Matos <jisapjisap@gmail.com>
2025-05-25 17:54:56 +0200

    blur effect on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
+"use client"
+
+
 import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import { HiBars3, HiMagnifyingGlass } from 'react-icons/hi2'
+import React, { useState } from 'react'
+import { HiBars3, HiMagnifyingGlass, HiShoppingBag, HiUser } from 'react-icons/hi2'
 
 type NavIconsProps = {
   className?: string,
@@ -12,24 +15,49 @@ type NavIconsProps = {
 
 const NavIcons = ({ className ="", tabIndex}: NavIconsProps) => (
   <div className={clsx("flex items-center gap-8", className)}>
-    <Link 
-      href="#" 
-      className='text-white' 
-      aria-label="Search" 
+    <Link
+      href="#"
+      className='text-white'
+      aria-label="Search"
       tabIndex={tabIndex}
     >
       <HiMagnifyingGlass size={24} />
+    </Link>
+    <Link 
+      href="#" 
+      className='text-white' 
+      aria-label="Account" 
+      tabIndex={tabIndex}
+    >
+      <HiUser size={24} />
+    </Link>
+    <Link 
+      href="#" 
+      className='text-white' 
+      aria-label="Account" 
+      tabIndex={tabIndex}
+    >
+      <HiShoppingBag size={24} />
     </Link>
   </div>
 )
 
 
 export const Navbar = () => {
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const toggleDrawer = () => setIsDrawerOpen(!isDrawerOpen)
+
+
   return (
     <header>
       <div className="navbar w-full fixed top-0 left-0 z-50 bg-black text-white">
         <div className='flex items-center justify-between p-2 md:p-4'>
-          <button className='p-2 cursor-pointer text-white transition-colors duration-300 hover:bg-white/20'>
+          <button 
+            onClick={toggleDrawer} 
+            aria-label="Menu" 
+            className='p-2 cursor-pointer text-white transition-colors duration-300 hover:bg-white/20'
+          >
             <HiBars3 size={24} />
           </button>
 
@@ -48,6 +76,18 @@ export const Navbar = () => {
           </div>
         </div>
       </div>
+
+
+      <div 
+        className={clsx(
+          "nav-drawer-blur fixed inset-0 z-40 bg-black/40 opacity-0 transition-all duration-500",
+          isDrawerOpen 
+            ? "pointer-events-auto opacity-100 backdrop-blur-xs"
+            : "pointer-events-none backdrop-blur-none"
+        )}
+        onClick={toggleDrawer}
+        aria-hidden="true"
+      />
     </header>
   )
 }
